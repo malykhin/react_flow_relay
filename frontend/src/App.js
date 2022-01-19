46088bccb75eb35c41f426f49c6ff9a37450332d
golovewkin <golovewkin@gmail.com>
2022-01-19 19:18:21 +0200

    1 request is ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,44 @@
+import React from 'react';
+import './App.css';
 import "./App.css";
+import fetchGraphQL from './relay/fetchGraphQL';
+import graphql from 'babel-plugin-relay/macro';
+import {
+  RelayEnvironmentProvider,
+  loadQuery,
+  usePreloadedQuery,
+} from 'react-relay/hooks';
+import RelayEnvironment from './relay/RelayEnvironment';
+import {ReactNode} from "react";
+const { Suspense } = React;
 
-function App() {
+// Define a query
+const AreaNameQuery = graphql`
+    query AppAreaNameQuery {
+        frontEnd {
+            id
+            name
+            skills {
+                edges {
+                    node {
+                        id
+                        name
+                    }
+                }
+            }
+        }
+    }
+`;
+
+// Immediately load the query as our app starts. For a real app, we'd move this
+// into our routing configuration, preloading data as we transition to new routes.
+const preloadedQuery = loadQuery(RelayEnvironment, AreaNameQuery, {
+  /* query variables */
+});
+
+function App(props) {
+  const data = usePreloadedQuery(AreaNameQuery, props.preloadedQuery);
+  console.log(data);
   return (
     <div className="App">
       <header># To Do</header>
@@ -8,4 +46,14 @@ function App() {
   );
 }
 
-export default App;
+function AppRoot() {
+  return (
+    <RelayEnvironmentProvider environment={RelayEnvironment}>
+      <Suspense fallback={'Loading...'}>
+        <App preloadedQuery={preloadedQuery} />
+      </Suspense>
+    </RelayEnvironmentProvider>
+  );
+}
+
+export default AppRoot;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import AppRoot from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const Root = () => (
   <React.StrictMode>
-    <App />
+    <AppRoot />
   </React.StrictMode>
 );
 
